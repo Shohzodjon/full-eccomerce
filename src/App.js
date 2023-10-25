d9e48ac808e90da704dd4a8868ef0244521e33b7
Shohzod <xorozovshohzod@gmail.com>
2023-10-25 05:39:49 +0300

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./assets/style/main.scss";
 import Navbar from "./sections/Navbar";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
     <section>
       <Navbar />
+      <Footer />
     </section>
   );
 }
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -26,8 +26,68 @@ const Footer = () => {
             </li>
           </ul>
           {/* first list */}
+          <ul>
+            <li>
+              <a href="#">
+                <b>Support</b>
+              </a>
+            </li>
+            <li>
+              <a href="#">111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</a>
+            </li>
+            <li>
+              <a href="mailto:exclusive@gmail.com">exclusive@gmail.com</a>
+            </li>
+            <li>
+              <a href="++88015-88888-9999">+88015-88888-9999</a>
+            </li>
+          </ul>
+          {/* second list */}
+          <ul>
+            <li>
+              <a href="#">
+                <b>Account</b>
+              </a>
+            </li>
+            <li>
+              <a href="#">My account</a>
+            </li>
+            <li>
+              <a href="#">Login / Register</a>
+            </li>
+            <li>
+              <a href="#">Cart</a>
+            </li>
+            <li>
+              <a href="#">Wishlist</a>
+            </li>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+          </ul>
+          {/* thirtd list */}
+          <ul>
+            <li>
+              <a href="#">
+                <b>Quick Link</b>
+              </a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#">Terms Of Use</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
   );
 };
+export default Footer;
